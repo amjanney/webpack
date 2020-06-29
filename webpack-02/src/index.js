7fe40454cd71843e35408098f679275f637a710d
郭建利 <guojianli@ihappy.net.cn>
2020-06-29 22:48:21 +0800

    add: 配置React打包环境

--- webpack-02/src/index.js
+++ webpack-02/src/index.js
@@ -1,5 +1,5 @@
 import '@babel/polyfill';
-import css from './style/index.less';
+import './style/index.less';
 // import pic from './images/logo.png';
 // import axios from 'axios';
 
@@ -9,32 +9,42 @@ import css from './style/index.less';
 // });
 
 // css HMR
-var btn = document.createElement('button');
-btn.innerHTML = '新增';
-document.body.appendChild(btn);
-btn.onclick = function () {
-  var div = document.createElement('div');
-  div.innerHTML = 'item';
-  document.body.appendChild(div);
-};
+// var btn = document.createElement('button');
+// btn.innerHTML = '新增';
+// document.body.appendChild(btn);
+// btn.onclick = function () {
+//   var div = document.createElement('div');
+//   div.innerHTML = 'item';
+//   document.body.appendChild(div);
+// };
 
 // js HMR
-import counter from './counter';
-import number from './number';
+// import counter from './counter';
+// import number from './number';
 
-counter();
-number();
+// counter();
+// number();
 
-if (module.hot) {
-  module.hot.accept('./number', function () {
-    document.body.removeChild(document.getElementById('number'));
-    number();
-  });
-}
+// if (module.hot) {
+//   module.hot.accept('./number', function () {
+//     document.body.removeChild(document.getElementById('number'));
+//     number();
+//   });
+// }
 
 // es6+
 
-const arr = [new Promise(() => {}), new Promise(() => {})];
-arr.map((item) => {
-  console.log(item);
-});
+// const arr = [new Promise(() => {}), new Promise(() => {})];
+// arr.map((item) => {
+//   console.log(item);
+// });
+
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+class App extends Component {
+  render() {
+    return <div>hello world</div>;
+  }
+}
+ReactDom.render(<App />, document.getElementById('app'));
